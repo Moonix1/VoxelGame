d85718ad1bdabe6fb301cbe11f8c826cc6ac5011
Moonix1 <bzsolczay@gmail.com>
2024-12-18 03:48:09 +1100

    Made a colourful square

--- src/main.rs
+++ src/main.rs
@@ -33,13 +33,15 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0],   color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0],  color: [0.0, 0.0, 1.0] },
+    Vertex { position: [0.0, 0.5, 0.0],    color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, 0.5, 0.0],   color: [0.0, 1.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0],  color: [0.0, 0.0, 1.0] },
+    Vertex { position: [0.0, -0.5, 0.0],   color: [0.0, 0.0, 0.0] },
 ];
 
 const INDICES: &[u16] = &[
     0, 1, 2,
+    2, 3, 0,
 ];
 
 struct App<'a> {
